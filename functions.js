dbb2bd245c122b181888d3ce4938b1f5dc9d89be
nathan <sourcebank@126.com>
2013-04-27 17:54:58 +0800

    functions changed

--- functions.js
+++ functions.js
@@ -1,5 +1,19 @@
+	/**
+	@param ele  Dom,
+	@param pro  String,
+	@return pro  String
+	*/
 	function getStyle(ele,pro){
 
+		if(ele.currentStyle){
+			return ele.currentStyle[pro];
+		}
+		else if(window.getComputedStyle||document.getDefaultView.getComputedStyle){
+			return (window.getComputedStyle||document.getDefaultView.getComputedStyle)[pro];
+		}
+		else{
+			return ele.style[pro];
+		}
 	}
 
 
